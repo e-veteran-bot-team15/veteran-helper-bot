972da6e1edec5537411ec1054134c8f7c8315054
lemoncat <xar4ak7even5@gmail.com>
2023-12-08 19:01:37 +0200

    initial commit

---
+++ src/main/java/hackathon/luxoft/eveterandemo/EveteranDemoApplication.java
@@ -0,0 +1,13 @@
+package hackathon.luxoft.eveterandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveteranDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveteranDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/luxoft/eveterandemo/EveteranDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.luxoft.eveterandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveteranDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
